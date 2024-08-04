1821c97bd35abd06620c5980d7a6098c2b33a306
Bpmodele <Bpmodele@users.noreply.github.com>
2024-08-04 10:52:00 -0400

    Fixed the apiUrl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,7 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={key}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
