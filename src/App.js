b2c79e2d69ddf6069ea6ff93921493a68b7b2be5
Bpmodele <Bpmodele@users.noreply.github.com>
2024-08-10 14:53:49 -0400

    Added noreferrer to my footer

--- src/App.js
+++ src/App.js
@@ -18,11 +18,16 @@ export default function App() {
             <a
               href="https://github.com/Bpmodele/dictionary-project"
               target="_blank"
+              rel="noreferrer"
             >
               GitHub
             </a>{" "}
             and hosted on{" "}
-            <a href="https://rad-entremet-42e384.netlify.app/" target="_blank">
+            <a
+              href="https://rad-entremet-42e384.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               Netlify
             </a>{" "}
           </small>
